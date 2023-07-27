bf2e8836e4af8ea83b0afd35e1a15fda199d0964
Andre Angelantoni <aangel@mac.com>
2023-07-27 16:24:43 -0700

    Update lambda_test.

--- tests/lambda_test.spec.js
+++ tests/lambda_test.spec.js
@@ -5,30 +5,30 @@
  */
 
 // Set up Playwright.
-const { test, expect } = require('@playwright/test')
+const { test, expect, chromium } = require('@playwright/test')
 
-// LambdaTest setup.
-(async () => {
-  const capabilities = {
-    'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-    'browserVersion': 'latest',
-    'LT:Options': {
-      'platform': 'Windows 10',
-      'build': 'Lambda Test 101',
-      'name': 'Lambda Test 101',
-      'user': process.env.LT_USERNAME,
-      'accessKey': process.env.LT_ACCESS_KEY,
-      'network': true,
-      'video': true,
-      'console': true
-    }
+const capabilities = {
+  'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+  'browserVersion': 'latest',
+  'LT:Options': {
+    'platform': 'Windows 10',
+    'build': 'Lambda Test 101',
+    'name': 'Lambda Test 101',
+    'user': process.env.LT_USERNAME,
+    'accessKey': process.env.LT_ACCESS_KEY,
+    'network': true,
+    'video': true,
+    'console': true
   }
+}
 
-  const browser = await chromium.connect({
-    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
-  })
 
+test.describe('Lambda tests.', () => {
   test('Scenario 1', async ({ page, context }) => {
+    const browser = await chromium.connect({
+      wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+
     await page.goto('https://www.lambdatest.com/selenium-playground')
     await page.click('text="Simple Form Demo"')
 
@@ -132,4 +132,4 @@ const { test, expect } = require('@playwright/test')
     const successString = await successMsg.textContent()
     await expect(successString).toBe("Thanks for contacting us, we will get back to you shortly.")
   })
-})()
+})
